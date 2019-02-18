9a1a85fdf015e2cbc36e7ac0344fa5d50871db04
Your Name <you@example.com>
2019-02-17 21:23:47 -0600

    temp vars must reference self * pull key from file

--- bindings/python/samples/weather.py
+++ bindings/python/samples/weather.py
@@ -21,21 +21,22 @@ class GraphicsTest(SampleBase):
 
     def __display_current(self):
         blue = graphics.Color(0, 0, 255)
-        text = temp.get('temp') + ' \u00b0'
+        text = self.temp.get('temp') + ' \u00b0'
         graphics.DrawText(self.matrix, self.font, 2, 10, blue, text)
 
     def __display_todays_low(self):
         red = graphics.Color(0, 0, 255)
-        text = temp.get('temp_min') + ' \u00b0'
+        text = self.temp.get('temp_min') + ' \u00b0'
         graphics.DrawText(self.matrix, self.font, 2, 10, red, text)
 
     def __display_todays_high(self):
         green = graphics.Color(0, 255, 0)
-        text = temp.get('temp_max') + ' \u00b0'
+        text = self.temp.get('temp_max') + ' \u00b0'
         graphics.DrawText(self.matrix, self.font, 2, 10, green, text)
 
     def __get_weather(self):
-        owm = pyowm.OWM('your-API-key')
+        key = open(".env","r").read().rstrip('\n')
+        owm = pyowm.OWM(key)
         observation = owm.weather_at_id(2643741) # Chicago
         self.temp = observation.get_weather().get_temperature('celsius')
 
