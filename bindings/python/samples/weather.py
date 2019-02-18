4db05c1e836d531a4abe2bd3835b7337d2de9aa6
Mark Richardson <mark@markeban.com>
2019-02-17 19:25:00 -0600

    get weather

--- bindings/python/samples/weather.py
+++ bindings/python/samples/weather.py
@@ -2,21 +2,42 @@
 from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
+import pyowm
 
 
 class GraphicsTest(SampleBase):
     def __init__(self, *args, **kwargs):
         super(GraphicsTest, self).__init__(*args, **kwargs)
+        self.canvas = self.matrix
+        self.font = graphics.Font()
 
     def run(self):
-        canvas = self.matrix
-        font = graphics.Font()
-        font.LoadFont("../../../fonts/7x13.bdf")
-
-        blue = graphics.Color(0, 0, 255)
-        graphics.DrawText(canvas, font, 2, 10, blue, "-11\u00b0")
-
+        # weather_data = self.__get_weather()
+        self.font.LoadFont("../../../fonts/7x13.bdf")
+        self.__display_current()
+        self.__display_todays_low()
+        self.__display_todays_high()
         time.sleep(10)   # show display for 10 seconds before exit
+
+    def __display_current(self):
+        blue = graphics.Color(0, 0, 255)
+        text = temp.get('temp') + ' \u00b0'
+        graphics.DrawText(self.canvas, self.font, 2, 10, blue, text)
+
+    def __display_todays_low(self):
+        red = graphics.Color(0, 0, 255)
+        text = temp.get('temp_min') + ' \u00b0'
+        graphics.DrawText(self.canvas, self.font, 2, 10, red, text)
+
+    def __display_todays_high(self):
+        green = graphics.Color(0, 255, 0)
+        text = temp.get('temp_max') + ' \u00b0'
+        graphics.DrawText(self.canvas, self.font, 2, 10, green, text)
+
+    def __get_weather(self):
+        owm = pyowm.OWM('your-API-key')
+        observation = owm.weather_at_id(2643741) # Chicago
+        self.temp = observation.get_weather().get_temperature('celsius')
 
 
 # Main function
