c2f655e1120f2a4c3aa6f26589171b4721df798e
Mark Richardson <mark@markeban.com>
2019-09-09 22:28:24 -0500

    disable_existing_loggers

--- bindings/python/samples/main.py
+++ bindings/python/samples/main.py
@@ -4,7 +4,7 @@ import logging.config
 from weather import Weather
 
 if __name__ == "__main__":
-    logging.config.fileConfig('mylogger.conf')
+    logging.config.fileConfig('mylogger.conf', disable_existing_loggers=False)
     logger = logging.getLogger('mylogger')
     weather = Weather()
     if (not weather.process()):
